0dc181244553365b3aa438e73ba1c78a35a81283
Parzi68 <anamsmit11@gmail.com>
2024-04-15 12:08:15 +0530

    changed configuration

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,11 +1,8 @@
 package com.example.demo;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo")
 public class MyController {
 
     @GetMapping("/public")
@@ -14,14 +11,14 @@ public class MyController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('user')")
+//    @PreAuthorize("hasRole('user')")
     public String hello() {
         return "Private endpoint accessible to users with 'User' role";
     }
 
     
     @GetMapping("/key")
-    @PreAuthorize("hasRole('admin')")
+//    @PreAuthorize("hasRole('admin')")
     public String privateEndpoint() {
     	return "Private endpoint accessible to users with 'Admin' role";
     }
--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,42 +1,47 @@
-package com.example.demo;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-import lombok.RequiredArgsConstructor;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-	private final JWTAuthConverter jwtAuthConverter = new JWTAuthConverter();
-	
-	@Bean
-	SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf(AbstractHttpConfigurer::disable);
-		httpSecurity.authorizeHttpRequests(req -> {
-			req.requestMatchers("/api/v1/demo", "/api/v1/demo/key").authenticated();
-			req.requestMatchers("/api/v1/demo/public").permitAll();
-		});
-		
-		httpSecurity.oauth2ResourceServer(token -> {
-			token.jwt( jwt -> {
-				jwt.jwtAuthenticationConverter(jwtAuthConverter);
-			});
-		});
-		
-		httpSecurity.sessionManagement(ses -> {
-			ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-				});
-//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		return httpSecurity.build();
-	}
-}
+//package com.example.demo;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//	private final JWTAuthConverter jwtAuthConverter = new JWTAuthConverter();
+//	
+//	@Bean
+//	SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+//		httpSecurity.csrf(AbstractHttpConfigurer::disable);
+//		httpSecurity.authorizeHttpRequests(req -> {
+//			req.requestMatchers("/", "/key").authenticated();
+//			req.anyRequest().permitAll();
+//		});
+//		
+//		httpSecurity.oauth2ResourceServer(token -> {
+//			token.jwt( jwt -> {
+//				jwt.jwtAuthenticationConverter(jwtAuthConverter);
+//			});
+//		});
+//		
+//		httpSecurity.oauth2Login(Customizer.withDefaults());
+//		httpSecurity.oauth2Client(Customizer.withDefaults());
+////		httpSecurity.formLogin(Customizer.withDefaults());
+//		
+//		httpSecurity.sessionManagement(ses -> {
+//			ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//				});
+////				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		return httpSecurity.build();
+//	}
+//}
